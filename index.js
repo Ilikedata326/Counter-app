35f8f4a3ba909ea5c1f4abae6716ff8475118234
Marwane Khadrouf <61570780+Ilikedata326@users.noreply.github.com>
2022-11-27 02:05:39 +0100

    Add counter app fles via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+console.log(saveEl)
+
+function increment() {
+    count += 1
+    countEl.innerText = count 
+} 
+
+function save(){
+    // Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let countStr = count + ' - ' 
+    // Render the variable in the saveEl using innerText
+    saveEl.textContent += countStr
+    // Initialize counter 
+    countEl.textContent = 0
+    count = 0
+           
+}
+ 
